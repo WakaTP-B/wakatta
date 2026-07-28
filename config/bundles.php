534486f785808f9f93a067656ebf6eedc92e131c
WakaTP-B <courcelles.amaury@orange.fr>
2026-07-28 11:45:31 +0200

    chore: log_statement=all (Postgres) + monolog-bundle pour debug

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
